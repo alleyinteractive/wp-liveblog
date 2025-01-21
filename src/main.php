aefecb08ea062e07987524725831674914f8434a
efuller <eric@alley.co>
2025-01-21 16:32:26 -0500

    Add in a liveblog post type

--- src/main.php
+++ src/main.php
@@ -8,13 +8,16 @@
 namespace Alley\WP\WP_Liveblog;
 
 use Alley\WP\Features\Group;
+use Alley\WP\WP_Liveblog\Features\WP_Liveblog_Post_Type_Liveblog;
 
 /**
  * Instantiate the plugin.
  */
 function main(): void {
 	// Add features here.
-	$plugin = new Group();
+	$plugin = new Group(
+		new WP_Liveblog_Post_Type_Liveblog(),
+	);
 
 	$plugin->boot();
 }
